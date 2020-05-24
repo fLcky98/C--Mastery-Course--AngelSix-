1818635a4002ba2a5d771d6e0d8012307b303ee2
Peter Bækgaard <peter.baekgaard@outlook.com>
2020-05-24 20:55:00 +0200

    Finished Methods

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetUserDateOfBirth("Peter Bækgaard"));
+            Console.WriteLine("The returned age is " + GetUserAge(21) + " years old");
+        }
+
+        public static string GetUserDateOfBirth(string fullName)
+        {
+            return fullName + " was born on Tuesday";
+        }
+
+        public static int GetUserAge(int userAge)
+        {
+            return userAge;
+        }
+    }
+}
