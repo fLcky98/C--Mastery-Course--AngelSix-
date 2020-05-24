103696d493184e7850722a0f26f8d737a8864fb0
Peter Bækgaard <peter.baekgaard@outlook.com>
2020-05-24 15:57:46 +0200

    Added Integers part

---
+++ Integers/Integers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //Declaring variable and assigning value
+            int counter = 5;
+
+            //Output the value of counter
+            Console.WriteLine("Counter: " + counter);
+
+            //Change value of counter to 7
+            counter = 7;
+
+            //Output the value of counter
+            Console.WriteLine("Counter: " + counter);
+
+
+            //Increment value of counter by 1 (++ = increment operator) (counter++ is the same as: counter = counter + 1) 
+            counter++;
+
+            //Output the value of counter
+            Console.WriteLine("Counter: " + counter);
+
+            //Same works for minus
+            counter--;
+
+            //Output the value of counter
+            Console.WriteLine("Counter: " + counter);
+
+        }
+
+    }
+}
