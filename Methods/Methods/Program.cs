a653104690dce70ab06405290869e10313c18c1e
Peter Bækgaard <peter.baekgaard@outlook.com>
2020-05-24 21:38:54 +0200

    Finished Var Keywords

--- Methods/Methods/Program.cs
+++ Methods/Methods/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Methods
 {
@@ -7,7 +8,7 @@ namespace Methods
         static void Main(string[] args)
         {
             Console.WriteLine(GetUserDateOfBirth("Peter Bækgaard"));
-            Console.WriteLine("The returned age is " + GetUserAge(21) + " years old");
+            Console.WriteLine($"The returned age is {GetUserAge(21)} years old");
         }
 
         public static string GetUserDateOfBirth(string fullName)
